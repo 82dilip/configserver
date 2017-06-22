2ba03d181fc067366734f872e95ad87b99734a09
82dilip <82.dilip@gmail.com>
2017-06-22 18:05:37 +0530

    Config ex

--- src/main/java/demo/ConfigServerApplication.java
+++ src/main/java/demo/ConfigServerApplication.java
@@ -2,13 +2,15 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+/*@Configuration
 @EnableAutoConfiguration
-@EnableDiscoveryClient
+@EnableDiscoveryClient*/
+@SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
